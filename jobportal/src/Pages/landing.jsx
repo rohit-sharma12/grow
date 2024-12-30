55e5ac2adee12bc96f8fcd5a61a8ee32529d50c3
rohitsharma <rs0043071@gmail.com>
2024-12-30 21:08:43 +0530

    change

--- jobportal/src/Pages/landing.jsx
+++ jobportal/src/Pages/landing.jsx
@@ -66,8 +66,6 @@ const LandingPage = () => {
         </CarouselContent>
       </Carousel>
 
-      <img src="public/banner.jpg" className="w-full" />
-
       <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <Card>
           <CardHeader>
